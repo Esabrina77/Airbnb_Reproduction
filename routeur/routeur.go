72cc26776bdcca7b0daf9fdaa67976a5717d7fd5
Esabrina77 <146923436+Esabrina77@users.noreply.github.com>
2024-06-21 23:16:39 +0200

    Update routeur.go

--- routeur/routeur.go
+++ routeur/routeur.go
@@ -11,7 +11,7 @@ func InitServe() {
     fileServer := http.FileServer(http.Dir("CSS"))
     http.Handle("/CSS/", http.StripPrefix("/CSS/", fileServer))
 
-    http.HandleFunc("/home", controller.HomeProduct)
+    http.HandleFunc("/", controller.HomeProduct)
     http.HandleFunc("/cabane", controller.CabaneProduct)
     http.HandleFunc("/wow", controller.WowProduct)
 
