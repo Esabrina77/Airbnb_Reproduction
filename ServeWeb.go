10f842c5146e98d358cab8107c53ba3c7b01575a
Esabrina77 <146923436+Esabrina77@users.noreply.github.com>
2024-06-21 22:07:37 +0200

    Update ServeWeb.go

--- ServeWeb.go
+++ ServeWeb.go
@@ -4,27 +4,16 @@ import (
 	"AIRBNB/routeur"
 	initTemplate "AIRBNB/templates"
 	"fmt"
-	"log"
-	"net/http"
-	"os"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080" // Fallback au port 8080 si la variable d'environnement PORT n'est pas définie
-	}
-
 	fmt.Println("server is running...")
-	fmt.Println("")
-	fmt.Printf("CLICK HERE to OPEN PAGE---> http://localhost:%s/home \n", port)
-	fmt.Println("")
-	fmt.Println("TO STOP THE SERVER, PRESS 'ctrl+C'")
 
+	fmt.Println("")
+	fmt.Print("CLICK HERE to OPEN PAGE---> http://localhost:8080/home \n")
+	fmt.Println("")
+	fmt.Println("TO STOP THE SERVER , PRESS  'ctrl+C' ")
 	initTemplate.InitTemplate()
-	
-	// Initialize your router and server
-	router := routeur.InitServe()
+	routeur.InitServe()
 
-	log.Fatal(http.ListenAndServe(":"+port, router))
 }
